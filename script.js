66a92d334f27b7d8954c31f9c68442d887722de0
Abdurashid <khaidarovabdurashid@gmail.com>
2024-10-06 19:33:41 +0000

    f

---
+++ script.js
@@ -0,0 +1,9 @@
+    let A, B, C, ans
+
+    A = Number(prompt('Write a number'))
+    B = Number(prompt('Write a number'))
+    C = Number(prompt('Write a number'))
+
+    ans = (A + B + C)/3 
+    
+    alert(ans)
